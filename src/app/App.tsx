e07785f76f9388881ec5043a7b56c04e78120f4f
Vitaly Yurkin <jvn9405@gmail.com>
2025-06-27 19:45:49 +0300

    Создал структуру папок и настроил алиасы

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Редактор Резюме</h1>
+    </>
+  );
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@app': '/src/app',
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@features': '/src/features',
+      '@shared': '/src/shared',
+      '@styles': 'src/styles',
+      '@types': '/src/types',
+      '@': '/src'
+    }
+  }
+});
