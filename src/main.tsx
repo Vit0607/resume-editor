030a16b6a448a5279f822aed732a60fb65de289c
Vitaly Yurkin <jvn9405@gmail.com>
2025-06-27 22:20:11 +0300

    Глобальные стили и CSS-переменные

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 
 import App from '@app/App';
 
-import './styles/index.scss';
+import './styles/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
